02adb1bfff33f09abde9e7cd6decfe8e5c9a18d9
BlackSamorez <andrei@panferov-sci.ru>
2022-01-04 20:57:30 +0300

    First test for debug_mode

--- tests/debug_mode/test_debug_mode.cpp
+++ tests/debug_mode/test_debug_mode.cpp
@@ -12,8 +12,10 @@
 TEST_CASE("Depth temp", "[raytracer]") {
     const std::string dir_path(PROGRAM_DIR);
 
-    CameraOptions camera_options(600, 400);
-    RenderOptions render_options{2, RenderMode::kDepth};
+    CameraOptions camera_options(500, 500);
+    camera_options.look_from = {-0.5, 1.5, 0.98};
+    camera_options.look_to = {0.0, 1.0, 0.0};
+    RenderOptions render_options{4, RenderMode::kDepth};
 
     auto image = Render(dir_path+ "classic_box/CornellBox-Original.obj", camera_options, render_options);
     image.Write(dir_path+ "classic_box/default.png");
